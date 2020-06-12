65b1d0d75f744d13b2c23fc7946b1526f1d3f372
shankar <habilelabs.io>
2020-06-12 12:11:03 +0530

    index

--- index.js
+++ index.js
@@ -3,123 +3,116 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {
-  NativeModules,
-  TextInput,
-  findNodeHandle,
-  AppRegistry,
-  View,
-  Text,
-  Platform
+    NativeModules,
+    TextInput,
+    findNodeHandle,
+    AppRegistry,
+    View,
+    Text,
+    Platform
 } from 'react-native';
 
 const { CustomKeyboardKit} = NativeModules;
 
 const {
-  install, uninstall,
-  insertText, backSpace, doDelete,
-  moveLeft, moveRight,
-  switchSystemKeyboard,
-  hideKeyboard,
-  setText,
-  getText,
-  hideStandardKeyboard
+    install, uninstall,
+    insertText, backSpace, doDelete,
+    moveLeft, moveRight,
+    switchSystemKeyboard,
+    hideKeyboard,
+    setText,
+    getText,
+    hideStandardKeyboard
 } = CustomKeyboardKit;
 
 export {
-  install, uninstall,
-  insertText, backSpace, doDelete,
-  moveLeft, moveRight,
-  switchSystemKeyboard,
-  hideKeyboard,
-  setText,
-  getText
+    install, uninstall,
+    insertText, backSpace, doDelete,
+    moveLeft, moveRight,
+    switchSystemKeyboard,
+    hideKeyboard,
+    setText,
+    getText
 };
 
 const keyboardTypeRegistry = {};
 
 export function register(type, factory) {
-  console.log('register')
-  keyboardTypeRegistry[type] = factory;
-}
-
-const RenderKeyboard = ({Comp, tag}) => {
-  console.log('RenderKeyboard')
-  return Comp({tag})
+    keyboardTypeRegistry[type] = factory;
 }
 
 class CustomKeyboardKitContainer extends Component {
-  render() {
-    console.log('CustomKeyboardKitContainer')
-    const {tag, type} = this.props;
-    const factory = keyboardTypeRegistry[type];
-    if (!factory) {
-      console.warn(`Custom keyboard type ${type} not registered.`);
-      return null;
+    render() {
+        const {tag, type} = this.props;
+        const factory = keyboardTypeRegistry[type];
+        if (!factory) {
+            console.warn(`Custom keyboard type ${type} not registered.`);
+            return null;
+        }
+        const Comp = factory();
+        return <Comp tag={tag} />;
     }
-    const Comp = factory;
-    return <RenderKeyboard Comp={Comp}tag={tag} />;
-  }
 }
 
 AppRegistry.registerComponent("CustomKeyboardKit", () => CustomKeyboardKitContainer);
 
 export class CustomTextInput extends Component {
-  static propTypes = {
-    ...TextInput.propTypes,
-    customKeyboardType: PropTypes.string,
-  }
+    static propTypes = {
+        ...TextInput.propTypes,
+        customKeyboardType: PropTypes.string,
+    }
 
-  componentDidMount() {
-    if (Platform.OS === 'ios') {
-      setTimeout(() => {
-        const nativeHandle = findNodeHandle(this.input)
-        if (nativeHandle) {
-          install(nativeHandle, 'empty');
+    componentDidMount() {
+        if (Platform.OS === 'ios') {
+            setTimeout(() => {
+                const nativeHandle = findNodeHandle(this.input)
+                if (nativeHandle) {
+                    install(nativeHandle, 'empty');
+                }
+            }, 100)
         }
-      }, 100)
     }
-  }
 
-  componentWillUnmount() {
-    if (Platform.OS === 'ios') {
-      uninstall(findNodeHandle(this.input))
+    componentWillUnmount() {
+        if (Platform.OS === 'ios') {
+            uninstall(findNodeHandle(this.input))
+        }
     }
-  }
 
-  // componentWillReceiveProps(newProps) {
-  //   if (newProps.customKeyboardType !== this.props.customKeyboardType) {
-  //     install(findNodeHandle(this.input), newProps.customKeyboardType);
-  //   }
-  // }
+    // componentWillReceiveProps(newProps) {
+    //   if (newProps.customKeyboardType !== this.props.customKeyboardType) {
+    //     install(findNodeHandle(this.input), newProps.customKeyboardType);
+    //   }
+    // }
 
-  onRef = ref => {
-    this.input = ref;
-  }
-
-  focus = () => {
-    if (this.input.__isMounted && !this.input.isFocused()) {
-      this.input.focus()
+    onRef = ref => {
+        this.input = ref;
     }
-  }
 
-  blur = () => {
-    if (this.input.__isMounted && this.input.isFocused()) {
-      this.input.blur()
+    focus = () => {
+        if (this.input.__isMounted && !this.input.isFocused()) {
+            this.input.focus()
+        }
     }
-  }
 
-  hideStandardKeyboard = () => {
-    hideStandardKeyboard(findNodeHandle(this.input))
-  }
+    blur = () => {
+        if (this.input.__isMounted && this.input.isFocused()) {
+            this.input.blur()
+        }
+    }
 
-  getTag = () => {
-    return findNodeHandle(this.input)
-  }
+    hideStandardKeyboard = () => {
+        hideStandardKeyboard(findNodeHandle(this.input))
+    }
 
-  render() {
-    const { customKeyboardType, ...others } = this.props;
-    return (
-      <TextInput {...others} ref={this.onRef} />
+    getTag = () => {
+        return findNodeHandle(this.input)
+    }
+
+    render() {
+        const { customKeyboardType, ...others } = this.props;
+        return (
+            <TextInput {...others} ref={this.onRef} />
     );
-  }
+    }
 }
